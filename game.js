ff5bee9e1e9ae0d7cf7b20bcc58fee67fdd22857
jygibson <jygibson922@gmail.com>
2018-09-12 08:38:27 -0400

    finally got counter to work, conditionals not working

--- game.js
+++ game.js
@@ -1,87 +1,65 @@
-var targetNumber = Math.floor((Math.random() * 120)+ 1);
+$(document).ready(function() {
+
+    //computer picks random target number of diamonds and logs it to the console
+var targetNumber = Math.floor((Math.random() * 101)+ 19);
 
 console.log(targetNumber);
 
-$("#random-number").text(targetNumber);
+//sets some variables for the counter, wins, and losses
 
 var counter = 0;
 var wins = 0;
 var losses = 0;
 
+//puts the targetNumber into the random-number span on the html
 
-// var numberOptions = Math.floor((Math.random() * 12));
+$("#random-number").text(targetNumber);
+
+//creates an array of images
 var diamonds = ["assets/images/bluediamond.jpg", "assets/images/diamond.jpg", "assets/images/pinkdiamond.jpg", "assets/images/purplediamond.jpg"];
-var count = 0;
 
+//function uses the array to place 4 images and assign each a random number
 function displayDiamondImages (){
     for (var i = 0; i < diamonds.length; i++){
         var randomValue = Math.floor(Math.random() * 12);
         $("#alldiamonds").append('<img class="diamond" src="' + diamonds[i] + '" data-diamond-value="'+ randomValue +'" width="200px">');
     }
 };
+//calls the function above
 displayDiamondImages();
 
-
+//on click event to handle the click event - creating the diamondValue and adding it to the counter. Fills in the total 
+//field in the html with the counter value
 $("#alldiamonds").on("click", '.diamond', function() {
     var diamondValue = parseInt($(this).attr("data-diamond-value"));
     counter += diamondValue;
-    alert(counter);
     console.log(counter)
+    $("#total").text(counter);
   });
 
-$("#total").text(counter);
-$("#wins").text(wins);
-$("#losses").text(losses);
+  // reset function we will call in our if else statements
+  function reset(){
+    targetNumber = Math.floor((Math.random() * 101)+ 19);
+    randomValue = Math.floor(Math.random() * 11) +1;
+    counter = 0;
+ }
 
+ //conditional statements for scoring
 if (counter === targetNumber) {
     alert("you win, girl. get yourself something shiny.");
     wins++;
+    reset();
 }
 
 else if (counter >= targetNumber) {
     alert("you lose. earn more money. bye");
     losses++;
-}
-// for (var p=0; p<numberOptions.length; p++){
-//     var pinkDiamond=$("<img>");
-//     pinkDiamond.addClass("pink-diamond");
-//     pinkDiamond.attr("src", "https://www.diamondportfolio.com.au/wp-content/uploads/2016/08/ic-what-pink-pink-1.jpg");
-//     pinkDiamond.attr("pink-value", numberOptions[p]);
-//     // $(".alldiamonds").append(pinkDiamond);
-// }
+    reset();
+};
 
-// $("#pink-diamond").on("click", function() {
-//     var pinkDiamondValue = ($(this).attr("pink-value"));
-//     pinkDiamondValue = parseInt(pinkDiamondValue);
-//     counter += pinkDiamondValue;
-//     if (counter === targetNumber){
-//         alert("you win");        
-//     }
-//     else if (counter >= targetNumber) {
-//         alert("you lose");
-//     }
-// });
-// for (var b=0; b<numberOptions.length; b++){
-//     var blueDiamond=$("<img>");
-//     blueDiamond.addclass("blue-diamond");
-//     blueDiamond.attr("src", "../images/bluediamond.jpg")
-//     blueDiamond.attr("blue-value", numberOptions[b]);
-//     $(".alldiamonds").append(blueDiamond);
-// }
+//updates all the text fields
+$("#total").text(counter);
+$("#wins").text(wins);
+$("#losses").text(losses);
 
-// for (var d=0; i<numberOptions.length; d++){
-//     var diamond=$("<img>");
-//     diamond.addclass("diamond");
-//     diamond.attr("src", "../images/diamond.jpg")
-//     diamond.attr("diamond-value", numberOptions[d]);
-//     $(".alldiamonds").append(diamond);
-// }
-
-// for (var l=0; l<numberOptions.length; l++){
-//     var purpleDiamond=$("<img>");
-//     purpleDiamond.addclass("purple-diamond");
-//     purpleDiamond.attr("src", "../images/purplediamond.jpg")
-//     purpleDiamond.attr("purple-value", numberOptions[l]);
-//     $(".alldiamonds").append(purpleDiamond);
-// }
-
+});
