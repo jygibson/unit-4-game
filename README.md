eefe129699578b89a26dcaf0c797c2e99ad5fa56
jygibson <jygibson922@gmail.com>
2018-09-12 22:32:12 -0400

    finished css, readme, alerts

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unit-4-game
 homework 4
+
+psychic-game
+This is homework assignment #4 for my coding class. The assignment is to create a game where the four images have mystery values, and you click on them to reach a goal value. if you reach the value, you win. If you go over the value, you lose. 
+
+It will combine html, css, javascript, and jQuery.
+
+First, I will psuedocode the game to figure out what I need to do, including the steps and variable.
+
+Second, I will write the html for what information is needed on the page, including game instructions, and the score.
+
+Third, I will create the javascript and jQuery specifically looking back upon the crystal-game in class activity. also got ideas, such as the image array, from the slideshow activity in class on 5.2 (I think?)
+
+Fourth, once it works, I will go back and make it prettier using css.
+
+Fifth, link to my portfolio site
+
+Sixth: perfecting the css
+
+Seventh, #nailedit. Will go back and try to finish the beginning of the hangman game, start the next trivia assignment, and study more
--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//nothing runs until html finishes loaded
 $(document).ready(function () {
 
     //computer picks random target number of diamonds and logs it to the console
@@ -20,6 +21,7 @@ $(document).ready(function () {
 
     //function uses the array to place 4 images and assign each a random number
     function displayDiamondImages() {
+        //.empty clears out any previous attributes for the images so we can use it to reset
         $("#alldiamonds").empty();
         for (var i = 0; i < diamonds.length; i++) {
             var randomValue = Math.floor(Math.random() * 12) + 1;
@@ -29,7 +31,7 @@ $(document).ready(function () {
     //calls the function above
     displayDiamondImages();
 
-    // reset function we will call in our if else statements
+    // reset function
     function resetGame() {
         targetNumber = Math.floor((Math.random() * 101) + 19);
         console.log(targetNumber);
@@ -39,7 +41,7 @@ $(document).ready(function () {
         displayDiamondImages();
     };
 
-    //on click event to handle the click event - creating the diamondValue and adding it to the counter. Fills in the total 
+    //on click event to handle the diamond click - creating the diamondValue and adding it to the counter. Fills in the total 
     //field in the html with the counter value
     $("#alldiamonds").on("click", '.diamond', function () {
         var diamondValue = parseInt($(this).attr("data-diamond-value"));
@@ -48,25 +50,17 @@ $(document).ready(function () {
         $("#total").text(counter);
         //conditional statements for scoring
         if (counter == targetNumber) {
-            alert("you win, girl. get yourself something shiny.");
+            alert("Yaaas, you win queen. Now treat yo self!!!");
             wins++;
             $("#wins").text(wins);
             resetGame();
         }
         else if (counter > targetNumber) {
-            alert("you lose. earn more money. bye");
+            alert("You lose. Click 'ok' to to try again.");
             losses++;
             $("#losses").text(losses);
             resetGame();
         };
     });
-
-
-    // //updates all the text fields
-    //puts the targetNumber into the random-number span on the html
-
-    // $("#random-number").text(targetNumber);
-    // $("#wins").text(wins);
-    // $("#losses").text(losses);
 
 });
